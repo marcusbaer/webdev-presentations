0cdd8ebc897a92c369405cc03ac95925353430b0
Marcus Baer <marcus.baer@publicispixelpark.de>
2020-02-14 15:16:27 +0100

    polyfilled lorem ipsum wip

---
+++ WEBCOMPONENTS/example/polyfilled/lorem-ipsum.js
@@ -0,0 +1,84 @@
+const tagName = 'lorem-ipsum';
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+        background-color: var(--bg-color, #eee);
+        display: block;
+        padding: var(--padding, 1rem);
+    }
+    :host([name]) {
+
+    }
+    ::slotted(*) {
+        border-left: 3px dashed var(--light-tree, red);
+        padding-left: 0.5rem;
+    }
+    h1 {
+        color: var(--shadow-tree, orange);
+    }
+    ::slotted(p) {
+        color: var(--shadow-tree, green);
+    }
+  </style>
+  <h1>[shadow tree h1]</h1>
+  <slot name="headline">[headline slot]</slot>
+  <div>
+    <slot>[unnamed slot]</slot>
+  </div>
+  <p id="name">your name here</p>
+`;
+
+window.ShadyCSS && window.ShadyCSS.prepareTemplate(template, tagName);
+
+class LoremIpsum extends HTMLElement {
+
+    constructor() {
+        super();
+        this.name = 'Default Name';
+    }
+
+    $(selector) {
+        return this.shadowRoot && this.shadowRoot.querySelector(selector);
+    }
+
+    connectedCallback() {
+        if (window.ShadyCSS) window.ShadyCSS.styleElement(this);
+
+        if (!this.shadowRoot) {
+            this.attachShadow({mode: 'open'});
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+            this.nameNode = this.$("#name");
+            this.name = this.getAttribute('name');
+        }
+
+        this.render();
+    }
+
+    static get observedAttributes() {
+        return ["name"];
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log(
+            `%cLoremIpsum - attribute changed: ${name} ${newVal}`,
+            "color: lightgray;"
+        );
+        this[name] = newVal;
+    }
+    
+    set name(value = '') {
+        if (this.getAttribute('name') !== value) this.setAttribute('name', value);        
+    }
+    
+    get name() {
+        return this.getAttribute('name').toUpperCase();
+    }
+
+    render() {
+        this.nameNode.textContent = this.name;
+    }
+}
+
+const register = () => customElements.define(tagName, LoremIpsum);
+window.WebComponents ? window.WebComponents.waitFor(register) : register();
---
+++ WEBCOMPONENTS/example/polyfilled/script.js
@@ -0,0 +1,16 @@
+/* global Prism, WebComponents  */
+
+const loremIpsum = document.querySelector('lorem-ipsum');
+const output = document.querySelector('#output');
+
+const updateMarkdown = (e) => 
+  output.innerHTML = Prism.highlight(
+    loremIpsum.shadowRoot.innerHTML,
+    Prism.languages.html,
+    'html'
+  );
+
+const observer = new MutationObserver(updateMarkdown);
+
+observer.observe(loremIpsum, { attributes: true, childList: true, subtree: true });
+WebComponents.waitFor(() => setTimeout(updateMarkdown));
